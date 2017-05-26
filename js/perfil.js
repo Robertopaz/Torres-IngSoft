dded57530185bb4ca727a6ec3a8f533c899b63b0
RPazPerez <robertopaz@msn.com>
2017-05-26 13:13:39 -0500

    Se agregan nuevos archivos
    se agrega el js para el perfil y el SQL para la base de datos

---
+++ js/perfil.js
@@ -0,0 +1,15 @@
+function loadProfile(argument) {
+	enviar=new XMLHttpRequest;
+	enviar.open('POST','php/usuario.php');
+	enviar.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	enviar.send('flag='+'load-profile'+'&u='+id);
+	enviar.onreadystatechange = function(){
+	  	if(enviar.readyState == 4 && enviar.status == 200){
+	  		respuesta=enviar.responseText;
+	  		if (respuesta=="Si") {
+	  			
+	  		}
+	  		
+	  	}
+	  }
+}
